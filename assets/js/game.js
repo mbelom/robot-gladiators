7bacdf9bf38e36e3cf3ae298843c60271174e888
Marissa Belombre <mbelo@MJBGALAXY>
2021-10-31 01:03:36 -0700

    inputted variables and logs

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,31 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+window.alert(playerName);
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+
+var playerName = "Tony the Robot";
+
+// Tony the Robot is ready for battle!
+console.log("Tony the Robot" + " is ready for battle!");
+
+// "Tony the Robot is ready for battle!"
+console.log(playerName + " is ready for battle!");
+
+// "Your robot, Tony the Robot, has won!
+console.log("Your robot, " + playerName + ", has won!");
+
+console.log("Tony the Robot" + " is ready for battle!");
+
+var name = "your name";
+console.log(name);
+//fight();
